9e8ae8f1370742fa8432ed245d13e554d5ac422b
mabreu2022 <mauricio.magosoft@gmail.com>
2025-07-11 09:02:44 -0300

    Primeiro commit

---
+++ ClientVCL.dpr
@@ -0,0 +1,14 @@
+program ClientVCL;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
